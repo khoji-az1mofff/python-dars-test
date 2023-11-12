2a6a67435ed91a1e777cae4a01dcd95b4611f7b5
Hojiakbar Azimov <147535410+khoji-az1mofff@users.noreply.github.com>
2023-11-13 00:34:14 +0500

    file yuklandi

---
+++ index.py
@@ -0,0 +1,48 @@
+# if else
+# avtolar=["naxia","audi","mers","tayota"]
+# for avto in avtolar:
+#     if avto == "mers":
+#         print(avto.upper())
+#     else:
+#         print(avto.title())
+
+# mevalar=["olma","bexi","nok","banan","tarvuz"] 
+# for meva in mevalar:
+#     if meva == "olma":
+#         print("men mazzali "+meva+" yeyishni hohlayman")
+
+# if 12 == 12:
+#     for i in range(12):
+#         print(i+1)
+# else:
+#     print("yo'q")
+
+# son=45
+# if 44+1 >=son and 15==15:
+#     print("ha")
+# else:
+#     print("yo'q")
+# il elif
+# yosh= int(input("yoshingizni kiriting = "))
+# if yosh <= 4:
+#     print("sizning yoshingiz "+str(yosh)+" da sizga kirish bepul.")
+# elif yosh <= 12:
+#     print("sizning yoshingiz "+str(yosh)+" da sizga kirish 5000 so'm.")
+# else:
+#     print("sizga kirish 10000 so'm.")
+
+# if 12==11 and 12<=12:
+#     print("12 = 12 ga")
+# else:
+#     print("12 katta 12 dan")
+
+ 
+
+
+
+
+
+       
+    
+
+    
