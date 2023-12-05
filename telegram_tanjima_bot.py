54d5b51f37f05e26629493e761a512cb069a94cc
Hojiakbar Azimov <147535410+khoji-az1mofff@users.noreply.github.com>
2023-12-05 21:02:59 +0500

    bot tayyor

--- telegram_tanjima_bot.py
+++ telegram_tanjima_bot.py
@@ -5,7 +5,18 @@ bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "Assalomu alaykum, Hush kelibsiz! Ozodbek aka ") 
+    javob= "Assalomu alaykum, Hush kelibsiz!"
+    javob+="\nMatn kiriting:" 
+    bot.reply_to(message,javob)
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    msg=message.text
+    if msg.isascii():
+        javob=to_cyrillic(msg)
+    else:
+        javob=to_latin(msg)
+    bot.reply_to(message, javob) 
 bot.infinity_polling()
 
 
