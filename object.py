5b152e3340d11976ee7d3bf2b83d123093220345
Hojiakbar Azimov <147535410+khoji-az1mofff@users.noreply.github.com>
2023-12-08 20:16:05 +0500

    2-dars tugadi

--- object.py
+++ object.py
@@ -1,19 +1,33 @@
-class Talaba:
-    def __init__(self,ism,familiya,tyil):
-        self.ism=ism
-        self.familiya=familiya
-        self.tyil=tyil
-    def tanishtir(self):
-        print(f"assalomu alaykum mening ismin {self.ism} familiyam {self.familiya} , yoshim {self.tyil}")
-    def get_name(self):
-        return self.ism
-    def get_age(self,yil):
-        return yil - self.tyil
-talaba1=Talaba("Hojiakbar","Azimov",2005)
-talaba2=Talaba("Ozodbek","Nematov",1998)
-talaba3=Talaba("Doniyorbek","Najmiddinov",2001)
-print(talaba2.get_name())
-print(talaba2.get_age(2023 ))
+# class Talaba:
+#     def __init__(self,ism,familiya,tyil):
+#         self.ism=ism
+#         self.familiya=familiya
+#         self.tyil=tyil
+#     def tanishtir(self):
+#         print(f"assalomu alaykum mening ismin {self.ism} familiyam {self.familiya} , yoshim {self.tyil}")
+#     def get_name(self):
+#         return self.ism
+#     def get_age(self,yil):
+#         return yil - self.tyil
+# talaba1=Talaba("Hojiakbar","Azimov",2005)
+# talaba2=Talaba("Ozodbek","Nematov",1998)
+# talaba3=Talaba("Doniyorbek","Najmiddinov",2001)
+# print(talaba2.get_name())
+# print(talaba2.get_age(2023 ))
+
+# class Fan():
+#     def __init__(self,nomi):
+#         self.nomi = nomi
+#         self.talabalar_soni=0
+#         self.talabalar=[]
+#     def add_talabalar(self,talaba):
+#         self.talabalar.append(talaba)
+#         self.talabalar_soni += 1
+#     def get_name(self):
+#         return self.nomi
+#     def get_students(self):
+#         return []
+        
 
         
     
