246fbb834454c7cf77b70b1361f042147e4e0045
Hojiakbar Azimov <147535410+khoji-az1mofff@users.noreply.github.com>
2023-12-08 20:04:05 +0500

    object 1-dars tugadi

--- object.py
+++ object.py
@@ -1,3 +1,19 @@
-def salom():
-    print("assalomu alaykum")
-print(type(salom))
+class Talaba:
+    def __init__(self,ism,familiya,tyil):
+        self.ism=ism
+        self.familiya=familiya
+        self.tyil=tyil
+    def tanishtir(self):
+        print(f"assalomu alaykum mening ismin {self.ism} familiyam {self.familiya} , yoshim {self.tyil}")
+    def get_name(self):
+        return self.ism
+    def get_age(self,yil):
+        return yil - self.tyil
+talaba1=Talaba("Hojiakbar","Azimov",2005)
+talaba2=Talaba("Ozodbek","Nematov",1998)
+talaba3=Talaba("Doniyorbek","Najmiddinov",2001)
+print(talaba2.get_name())
+print(talaba2.get_age(2023 ))
+
+        
+    
